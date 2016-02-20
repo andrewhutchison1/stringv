c2d2805d017494e93d2fdd10f6dd6ae0e1aae745
Andrew Hutchison <andrew@andrewhutchison.net>
2016-02-21 08:49:46 +1000

    Remove stringv_capacity implementation, add count field in stringv

--- stringv.c
+++ stringv.c
@@ -9,30 +9,16 @@ int stringv_init(
         size_t size,
         size_t blocksize)
 {
-    if (!stringv || !buf || size <= 1 || blocksize <= 1) {
+    if (!stringv || !buf || size <= 1 || blocksize <= 1 || blocksize > size) {
         return 0;
     }
 
     stringv->buf = buf;
     stringv->size = size;
     stringv->blocksize = blocksize;
+    stringv_count = 0;
 
     /* Fill buffer with zero */
     memset(stringv->buf, 0, size);
-
     return 1;
-}
-
-int stringv_capacity(struct stringv const *stringv)
-{
-    if (!stringv) {
-        return 0;
-    }
-
-    /* The optimum capacity for a given stringv is acheived when the length
-     * of all strings is less than the block size. We also have to keep in
-     * mind that there may be dead space at the end of the stringv's buffer.
-     * This is essentially the algebraic quotient of the buffer size and
-     * block size. */
-    return stringv->size / stringv->blocksize;
 }
