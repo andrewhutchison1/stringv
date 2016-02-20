dd0dfd3f91f7c40b9070dcd9d9c03fb5f4e44221
Andrew Hutchison <andrew@andrewhutchison.net>
2016-02-20 14:23:51 +1000

    Initial commit

---
+++ stringv.h
@@ -0,0 +1,20 @@
+#ifndef STRINGV_H_
+#define STRINGV_H_
+
+struct stringv {
+    char *buf;          /* Pointer to buffer; mutable */
+    size_t size;        /* The size of the buffer (including terminating \0 */
+    size_t blocksize;   /* The block size */
+};
+
+/* Initialises a stringv, returns 0 on failure */
+int stringv_init(
+        struct stringv *stringv,    /* stringv object to initialise */
+        char *buf,                  /* The buffer to use, must be writeable */
+        size_t size,                /* The size of the buffer, in chars */
+        size_t blocksize);          /* The desired block size */
+
+/* Determines the capacity of a stringv. Returns 0 on failure */
+int stringv_capacity(struct stringv const *stringv);
+
+#endif /* STRINGV_H_ */
