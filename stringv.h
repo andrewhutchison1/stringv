b6507817d926f6c30d803ed6c52c0742a92115d5
Andrew Hutchison <andrew@andrewhutchison.net>
2016-02-22 07:48:08 +1000

    Amend all public functions to have optional error parameter

--- stringv.h
+++ stringv.h
@@ -21,14 +21,16 @@ int stringv_init(
         struct stringv *stringv,    /* stringv object to initialise */
         char *buf,                  /* The buffer to use, must be writeable */
         unsigned buf_size,          /* The size of the buffer, in chars */
-        unsigned block_size);       /* The desired block size */
+        unsigned block_size,        /* The desired block size */
+        enum stringv_error *error);
 
 /* Copies a stringv to another stringv. Returns 1 on success and 0 on failure.
  * You can use this to copy the contents of a full stringv to another stringv
  * with a larger buffer. If the function fails, no work is done */
 int stringv_copy(
         struct stringv *dest,       /* stringv to copy to */
-        struct stringv *source);    /* stringv to copy from */
+        struct stringv *source,     /* stringv to copy from */
+        enum stringv_error *error);
 
 /* Pushes a string to the end of the stringv. Returns 1 on success, 0 on
  * failure. The index of the newly created string element can be recovered
@@ -36,7 +38,7 @@ int stringv_copy(
 int stringv_push_back(
         struct stringv *stringv,
         char const *string,
-        unsigned string_length);
-
+        unsigned string_length,
+        enum stringv_error *error);
 
 #endif /* STRINGV_H_ */
