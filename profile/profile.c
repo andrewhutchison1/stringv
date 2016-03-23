986bd7610f9700c868e9112d3d86dab0701a94d5
Andrew Hutchison <andrew@andrewhutchison.net>
2016-03-23 10:27:03 +1000

    Write profile for iteration

--- profile/profile.c
+++ profile/profile.c
@@ -1,14 +1,43 @@
-#include "bench.h"
+#include "profile.h"
 
 #include <assert.h>
 #include <string.h>
 #include <stdlib.h>
 
-char const *read_strings(char const *filename)
+#include "../stringv.h"
+
+struct stringv *init_stringv(struct stringv *s, int block_size)
 {
+    char *data = calloc(ALLOCATION_SIZE, 1);
+    assert(data);
+    assert(s);
+
+    return stringv_init(s, data, ALLOCATION_SIZE, block_size);
+}
+
+struct stringv *fill_stringv(struct stringv *s, struct string_buf buf)
+{
+    char const *begin = NULL;
+    int i = 0;
+    
+    assert(s);
+    assert(buf.buf && buf.size > 0);
+
+    begin = buf.buf;
+    for (i = 0; i < buf.size; ++i) {
+        if (!buf.buf[i]) {
+            stringv_push_back(s, begin, (buf.buf + i) - begin);
+            begin = buf.buf + i + 1;
+        }
+    }
+
+    return s;
+}
+
+struct string_buf read_strings(char const *filename)
+{
+    struct string_buf sbuf = {NULL, 0};
     FILE *file = NULL;
-    char *content = NULL;
-    size_t size = 0;
 
     /* Try to open the file */
     assert(filename);
@@ -17,16 +46,45 @@ char const *read_strings(char const *filename)
 
     /* Try to determine the file size */
     fseek(file, 0, SEEK_END);
-    size = ftell(file);
+    sbuf.size = ftell(file) + 1;
     rewind(file);
 
     /* Try to allocate a buffer to store the file contents */
-    content = calloc(size + 1, 1);
-    assert(content);
+    sbuf.buf = calloc(sbuf.size, 1);
+    assert(sbuf.buf);
 
     /* Read the file data into the buffer */
-    fread(content, 1, size, file);
+    fread(sbuf.buf, 1, sbuf.size - 1, file);
     fclose(file);
 
-    return content;
+    /* NUL terminate */
+    sbuf.buf[sbuf.size] = '\0';
+
+    return sbuf;
+}
+
+struct string_buf terminate_at_newline(struct string_buf sbuf)
+{
+    int i = 0;
+    assert(sbuf.buf);
+
+    for (i = 0; i < sbuf.size; ++i) {
+        if (sbuf.buf[i] == '\n') {
+            sbuf.buf[i] = '\0';
+        }
+    }
+
+    return sbuf;
+}
+
+void report_profile_results(
+        struct stringv const *s,
+        struct string_buf const sbuf,
+        clock_t clock)
+{
+    assert(s);
+
+    printf("Profile completed:\n\tTime %lu\n\tBlock size %d\n\tBlock used "
+            "%d/%d\n\tData size %d\n", clock, s->block_size,
+            s->block_used, s->block_total, sbuf.size);
 }
--- profile/profile.h
+++ profile/profile.h
@@ -1,11 +1,34 @@
-#ifndef BENCH_H_
-#define BENCH_H_
+#ifndef PROFILE_H_
+#define PROFILE_H_
 
 #include <stdio.h>
+#include <time.h>
 
-#define ALLOCATION_SIZE (4*1024)
+#define ALLOCATION_SIZE (4*1024*1024)
 
-/* Reads strings from stdin into a malloc'd buffer */
-char const *read_strings(void);
+struct string_buf {
+    char *buf;
+    int size;
+};
 
-#endif /* BENCH_H_ */
+/* Initialises a stringv with the given block size and a 'big enough'
+ * buffer */
+struct stringv *init_stringv(struct stringv *s, int block_size);
+
+/* Fills a stringv with data from a string_buf */
+struct stringv *fill_stringv(struct stringv *s, struct string_buf buf);
+
+/* Reads strings from the given file into a dynamically allocated buffer. */
+struct string_buf read_strings(char const *filename);
+
+/* Given a buffer full of strings separated by newlines, replace each newline
+ * character with the NUL terminator \0 */
+struct string_buf terminate_at_newline(struct string_buf sbuf);
+
+/* Reports the profile results TODO json */
+void report_profile_results(
+        struct stringv const *s,
+        struct string_buf const sbuf,
+        clock_t clock);
+
+#endif /* PROFILE_H_ */
